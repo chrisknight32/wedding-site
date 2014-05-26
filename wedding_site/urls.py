8461511717a17d32d5be6e0a7a3f814f7d5b4b30
Ubuntu <ubuntu@ip-172-31-12-180.us-west-2.compute.internal>
2014-05-26 06:26:06 +0000

    Allow the base site to access the wedding site.

--- wedding_site/urls.py
+++ wedding_site/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'wedding_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('home.urls')),
     url(r'^wedding/', include('home.urls')),
     url(r'^rsvp/', include('rsvp.urls')),
     url(r'^admin/', include(admin.site.urls)),
